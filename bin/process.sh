0920e9b73d4aa8fb8801db4af8cf989444962d71
M Hamza Siddiqui <mhamzasite@gmail.com>
2018-03-05 18:15:18 +0500

    42 version support
    Salesforce Data Loader Version 42.0 Upgrade/Support.

--- bin/process.sh
+++ bin/process.sh
@@ -26,6 +26,6 @@ if [ "$JAVA" == "" ]; then
   echo "Unable to find Java Runtime in PATH"
   exit -1
 else
-  $JAVA -cp dataloader-36.0.0-uber.jar -Dsalesforce.config.dir=$CONF_DIR com.salesforce.dataloader.process.ProcessRunner process.name=$PROCESS
+  $JAVA -cp dataloader-42.0.0-uber.jar -Dsalesforce.config.dir=$CONF_DIR com.salesforce.dataloader.process.ProcessRunner process.name=$PROCESS
 fi
 
