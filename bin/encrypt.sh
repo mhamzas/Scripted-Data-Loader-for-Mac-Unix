871a7ff4e764954f22c51d55a6a55894795566f4
M Hamza Siddiqui <mhamzasite@gmail.com>
2022-01-25 16:09:07 +0500

    Update encrypt.sh

--- bin/encrypt.sh
+++ bin/encrypt.sh
@@ -6,5 +6,5 @@ if [ "$JAVA" == "" ]; then
         echo "Unable to find Java Runtime in PATH";
         exit -1;
 else
-	$JAVA -cp dataloader-46.0.0-uber.jar com.salesforce.dataloader.security.EncryptionUtil "$@" | sed 's/^.*) \- //g'
+	$JAVA -cp dataloader-54.0.0-uber.jar com.salesforce.dataloader.security.EncryptionUtil "$@" | sed 's/^.*) \- //g'
 fi
